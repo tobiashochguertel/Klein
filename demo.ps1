41bdcb649d9bc5896054b85527bc3c58d3953e17
AdarshAS007 <adarshas107@gmail.com>
2026-03-06 20:09:57 -0800

    Delete demo.ps1

--- demo.ps1
+++
@@ -1,8 +0,0 @@
-# Demo script to print the Klein ASCII logo
-
-Write-Host "oooo   oooo ooooo       ooooooooooo ooooo oooo   oooo "
-Write-Host " 888  o88    888         888    88   888   8888o  88  "
-Write-Host " 888888      888         888ooo8     888   88 888o88  "
-Write-Host " 888  88o    888      o  888    oo   888   88   8888  "
-Write-Host "o888o o888o o888ooooo88 o888ooo8888 o888o o88o    88  "
-Write-Host "                                                      "
